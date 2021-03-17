7b54dfdce14100da012aa321a534094a4c928dbe
Nils Bokermann <nils.bokermann@bermuda.de>
2021-03-17 19:25:44 +0100

    Algolia Search Box (CSS and JS)

---
+++ static/js/algolia.js
@@ -0,0 +1,7 @@
+algoliasearchNetlify({
+    appId: '7GDB5748NU',
+    apiKey: '6a65db4289210ed835d4aa778cf51bde',
+    siteId: '9d5c7e45-27a5-4924-b769-6f1f7ac20a9e',
+    branch: 'master',
+    selector: 'div#search',
+});
